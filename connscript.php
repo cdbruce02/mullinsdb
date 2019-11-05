547537ce020f62f82db4701ac0dc9755a3546fd9
cdbruce02 <bruce.clay02@gmail.com>
2019-11-05 02:09:16 +0000

    Added prepared statments and output is now in table

--- connscript.php
+++ connscript.php
@@ -14,32 +14,38 @@ $link = mysqli_connect($dbhost, $username, $password, $dbname, $dbport);
 if($link === false){
 	echo "Connection failed";
 } else {
-	echo "Successfully connected!";
+	echo "Successfully connected!" . "<br>";
 }
-*/
+ */
 
-$search = $_POST["searchreq"];
+//SQL Query for search bar
+$sql = "SELECT ItemID, ItemName, QTY, zone, subsection FROM full_display WHERE ItemName LIKE ?";
 
-$sql = "SELECT * FROM full_display WHERE ItemName LIKE '%" . $search . "%'";
-$raw_result = mysqli_query($link, $sql);
+$stmt = $link->prepare($sql);
+	//Pass Searchreq post request
+	$search = '%'.$_POST["searchreq"].'%';
+	$stmt->bind_param("s", $search);
+	// Execute query
+	$stmt->execute();
+	$result = $stmt->get_result();
 
-if((mysqli_num_rows($raw_result)) > 0) {
-	//output data from each row as long as there is a result form the database
-	while($row = mysqli_fetch_assoc($raw_result)) {
-		//printf ("%s (%s) \n,", $row["ItemID"], $row["ItemName"], $row["QTY"], $row["zone"],$row["subsection"]);
-		echo $row['ItemID'];
-		echo $row['ItemName'];
-		echo $row['QTY'];
-		echo $row['zone'];
-
+	if ($result->num_rows > 0){
+	echo "<table><tr><th>Item ID</th><th>Item Name</th><th>Quantity</th><th>Location</th></tr>";	
+	while($row = $result->fetch_assoc()) {
+		echo "<tr><td>".$row["ItemID"] ."</td><td>" . $row["ItemName"] . "</td><td>" . $row[QTY] . "</td><td>" . $row["zone"] . " " . $row["subsection"] . "</td></tr>";
 	}
-} else {
-	echo ("no results :(");
-}
+	} else {
+		echo "Couldn't find anything";
+	}
+	
 
+//output data from each row as long as there is a result from the database
+
+/*
+	echo "Item ID: " . $row['ItemID'] . ".  Item Name: " . $row['ItemName'] . ".  Quantity: " . $row['QTY'] . ".  Location: " . $row['zon
 // Free Result Set
-mysql_free_result($raw_result);
 
+ */
 mysqli_close($link);
 
 ?>
